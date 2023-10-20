c07654151fb18eb9da72f72c74e3e2acd42bf275
KrankerApfel <t.rakoto76@gmail.com>
2023-10-20 17:10:20 +0200

    update main page info

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,25 @@
+import { Fragment } from "react"
+
 function App()
 {
-  return <h1>Studio Andromeda website is coming soon...</h1>
+  return <Fragment>
+         <h1>Studio Andromeda</h1>
+         <h2>Black UTAU voicers wanted for Project A.E.G.I.S!</h2>
+
+         <h3>Wanna be our next voicer ?</h3>
+         <a href="https://www.castingcall.club/projects/test-f15e063b-2460-41b9-8745-4c5eb82f4e11">
+         apply for the casting now !</a>
+
+          <h3>About us</h3>
+          <p>
+          Studio Andromeda is a Vocal Synth production team made of all Black volunteers.
+          The aim of Studio Andromeda is to bring more Black representation into the world
+          of Vocal Synth via UTAU. 
+          Starting with The A.E.G.I.S. Project.
+          </p>
+        
+        
+         </Fragment> 
 }
 
 export default App
